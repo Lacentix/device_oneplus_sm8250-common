94f946d1e6cc18bc484779be9a31dcbe6f3e320f
Yuan Si <do4suki@gmail.com>
2023-06-27 22:39:48 +0800

    sm8250-common: Sync with gps-legacy component
    Change-Id: I30fbd7b476dcc7fb3a56a52b44416d3c7a6d55fb
    Signed-off-by: Yuan Si <do4suki@gmail.com>

--- common.mk
+++ common.mk
@@ -96,9 +96,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
-# GPS
-LOC_HIDL_VERSION = 4.0
-
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
